a3038bfd30d0dd4d8a20d372421a123ca546a678
nah5f4 <132862882+nah5f4@users.noreply.github.com>
2023-06-17 18:34:24 -0300

    Create by.js

---
+++ by.js
@@ -0,0 +1,11 @@
+let tudo = document.querySelector("#tudo");
+let dark = document.querySelector("#dark");
+
+function light() {
+  tudo.classList.toggle("white");
+  if (dark.innerText === "Light Mode") {
+    dark.innerText = "Dark Mode";
+  } else if (dark.innerText === "Dark Mode") {
+    dark.innerText = "Light Mode";
+  }
+}
