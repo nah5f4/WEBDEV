4bcc3725dfa3669731b663a6d673e3f7e82b47d7
Naili Lucia <132862882+nah5f4@users.noreply.github.com>
2023-07-21 23:40:31 -0300

    Update by.js

--- by.js
+++ by.js
@@ -9,3 +9,28 @@ function light() {
     dark.innerText = "Light Mode";
   }
 }
+let img = document.querySelector("img");
+let name = document.querySelector("#nome");
+let user = document.querySelector("#user");
+let bio = document.querySelector("#bio");
+let link = document.querySelector("#link");
+async function busca() {
+  let res = await fetch("https://api.github.com/users/nah5f4");
+  console.log(res);
+  let json = await res.json();
+
+  img.src = json.avatar_url;
+
+  user.innerText = json.login;
+  name.innerText = json.name;
+  bio.innerText = json.bio;
+    const linkElement = document.createElement("a");
+
+
+  linkElement.href = json.html_url;
+
+  linkElement.textContent = "nah5f4";
+
+  link.appendChild(linkElement);
+}
+busca()
